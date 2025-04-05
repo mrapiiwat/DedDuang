47ed435b3b84078e17fa54f5c9103229e21feb5a
Apiwat <mrapiiwat@gmail.com>
2025-04-05 12:07:34 +0700

    feat: add express-rate-limit middleware to limit requests per IP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 
 //Swagger Setup
 import { setupSwagger } from "./common/config/swagger";
@@ -14,6 +15,7 @@ import authRoute from "./modules/auth/routes/auth.route";
 const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 const app = express();
 
+// Middleware
 app.use(cors());
 dotenv.config();
 app.use(morgan("dev"));
@@ -24,10 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 // Setup Swagger
 setupSwagger(app);
 
-//Routes
-app.use("/api", newsRoute);
-app.use("/api", authRoute);
-
 // 404 Error Handler
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
@@ -36,6 +34,20 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     suggestion: `Check the URL or refer to the API documentation at http://localhost:${port}/api-docs.`,
   });
 });
+
+// Limit requests per IP
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10,
+  message: "Too many requests, please try again later.",
+});
+
+app.use(limiter); // Apply rate limiting to all requests
+
+//Routes
+app.use("/api", newsRoute);
+app.use("/api", authRoute);
+
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
